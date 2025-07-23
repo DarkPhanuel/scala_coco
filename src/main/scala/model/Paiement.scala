ed723b6d95c91716c1d1ec4a0cca860a09f7c4d2
DouaaElKheir <douaaelkheir6@gmail.com>
2025-07-23 12:18:37 +0200

    Paiement

---
+++ src/main/scala/model/Paiement.scala
@@ -0,0 +1,11 @@
+package model
+
+import java.sql.Timestamp
+
+case class Paiement(
+                     id: Int = 0,
+                     numeroTransaction: String,
+                     montant: BigDecimal,
+                     statut: String = "en_attente",
+                     datePaiement: Option[Timestamp] = None
+                   )
