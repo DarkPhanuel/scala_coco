62117ee840182da44e3b440e57dc2dfd7d1d5b73
DouaaElKheir <douaaelkheir6@gmail.com>
2025-07-23 13:54:42 +0200

    Create PaiementDAO.scala

---
+++ src/main/scala/dao/PaiementDAO.scala
@@ -0,0 +1,67 @@
+package dao
+
+import java.sql.{Connection, Timestamp}
+import scala.collection.mutable.ListBuffer
+import models.Paiement
+import java.time.LocalDate
+
+object PaiementDAO {
+
+  def create(p: Paiement)(implicit conn: Connection): Boolean = {
+    val sql =
+      """
+        |INSERT INTO paiements (numero_transaction, montant, statut, date_paiement)
+        |VALUES (?, ?, ?, ?)
+        |""".stripMargin
+    val stmt = conn.prepareStatement(sql)
+    stmt.setString(1, p.numeroTransaction)
+    stmt.setBigDecimal(2, p.montant.bigDecimal)
+    stmt.setString(3, p.statut)
+
+    // Gestion de datePaiement optionnelle
+    val ts: Timestamp = p.datePaiement match {
+      case Some(date) => Timestamp.valueOf(date.atStartOfDay())
+      case None       => null
+    }
+    stmt.setTimestamp(4, ts)
+
+    stmt.executeUpdate() > 0
+  }
+
+  def getAll()(implicit conn: Connection): List[Paiement] = {
+    val stmt = conn.prepareStatement("SELECT * FROM paiements")
+    val rs = stmt.executeQuery()
+    val result = ListBuffer[Paiement]()
+
+    while (rs.next()) {
+      val dateOpt: Option[LocalDate] = Option(rs.getTimestamp("date_paiement")).map(_.toLocalDateTime.toLocalDate)
+
+      result += Paiement(
+        id = rs.getInt("id"),
+        numeroTransaction = rs.getString("numero_transaction"),
+        montant = rs.getBigDecimal("montant"),
+        statut = rs.getString("statut"),
+        datePaiement = dateOpt
+      )
+    }
+    result.toList
+  }
+
+  def findByNumero(numero: String)(implicit conn: Connection): Option[Paiement] = {
+    val stmt = conn.prepareStatement("SELECT * FROM paiements WHERE numero_transaction = ?")
+    stmt.setString(1, numero)
+    val rs = stmt.executeQuery()
+
+    if (rs.next()) {
+      val dateOpt = Option(rs.getTimestamp("date_paiement")).map(_.toLocalDateTime.toLocalDate)
+
+      Some(Paiement(
+        id = rs.getInt("id"),
+        numeroTransaction = rs.getString("numero_transaction"),
+        montant = rs.getBigDecimal("montant"),
+        statut = rs.getString("statut"),
+        datePaiement = dateOpt
+      ))
+    } else None
+  }
+}
