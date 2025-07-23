cba892d27b2f6ab0f511d90495b7260a3d3af129
WOANA Joseph <88871418+DarkPhanuel@users.noreply.github.com>
2025-07-23 13:09:25 +0200

    feat: utilisateur and vehicule

--- src/main/scala/DB/DB.scala
+++ src/main/scala/DB/DB.scala
@@ -1,6 +1,8 @@
-import java.sql.{Connection, DriverManager}
-object DB {
+package DB
 
+import java.sql.{Connection, DriverManager}
+
+object DB {
   var connection: Connection = null
   private val url = "jdbc:postgresql://ep-blue-dust-a2s88sx9-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
   private val username = "neondb_owner"
---
+++ src/main/scala/dao/VehiculeDAO.scala
@@ -0,0 +1,47 @@
+package dao
+
+import DB.DB
+
+object VehiculeDAO {
+
+ def addVehicule(vehicule: models.Vehicule): Boolean = {
+    val query = "INSERT INTO vehicules (immatriculation,marque, modele, nombrePlaces, annee, statut) VALUES (?, ?, ?, ?, ?, ?)"
+    val statement = DB.connection.prepareStatement(query)
+    statement.setString(1, vehicule.marque)
+    statement.setString(2, vehicule.modele)
+    statement.setInt(3, vehicule.nombrePlaces)
+    statement.setInt(4, vehicule.annee)
+    statement.setString(5, vehicule.immatriculation)
+    statement.setString(6, vehicule.statut)
+    try {
+      statement.executeUpdate() > 0
+    } catch {
+      case e: Exception =>
+        e.printStackTrace()
+        false
+    }
+  }
+
+ def getVehicules : Seq[models.Vehicule] = {
+    val query = "SELECT * FROM vehicules"
+    val statement = DB.connection.prepareStatement(query)
+    val resultSet = statement.executeQuery()
+    var vehicules: Seq[models.Vehicule] = Seq()
+
+    while (resultSet.next()) {
+      val vehicule = models.Vehicule(
+        id = resultSet.getInt("id"),
+        immatriculation = resultSet.getString("immatriculation"),
+        marque = resultSet.getString("marque"),
+        modele = resultSet.getString("modele"),
+        nombrePlaces = resultSet.getInt("nombrePlaces"),
+        annee = resultSet.getInt("annee"),
+        statut = resultSet.getString("statut")
+      )
+      vehicules :+= vehicule
+    }
+
+    vehicules
+  }
+
+}
